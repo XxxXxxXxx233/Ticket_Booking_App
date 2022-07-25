35c679ddf94cdfb66b6ba3730d93c4bf583090c0
XxxXxxXxx233 <lc18038037737@gmail.com>
2022-07-25 20:48:51 +0800

    auth setup

---
+++ auth/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import {json} from 'body-parser'
+
+const app = express();
+const port = 3000;
+
+app.use(json());
+
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hi there!');
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}!`)
+});
